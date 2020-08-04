0d9099970ccb80b3314380b14025e537bf191ee2
Alison Rosenblum <rosenbla@purdue.edu>
2020-08-04 15:19:02 -0400

    standard product developed

--- demazure.py
+++ demazure.py
@@ -1,1 +1,100 @@
 #!/usr/bin/env python3
+from string import ascii_lowercase
+
+
+def identify_n(word):
+    """
+    identifies the minimal n for which word is in $S_n$
+
+    Parameters
+    ----------
+    word : list or tuple
+        elements refer to generators $s_1,s_2,...$
+
+    Returns
+    ----------
+    n : int
+        one greater than the largest integer appearing in word
+
+    Examples
+    ----------
+    >>> identify_n((1,0,1,2))
+    3
+
+    >>> identify_n([1,2,3,4,5,4,3])
+    6
+    """
+    return max(word)+1
+
+def ob_join(separated):
+    """
+    joins a list of objects into a single object
+
+    Paramenters
+    ----------
+    separated : list
+        all elements strings
+
+    Returns
+    ----------
+    str
+        the concatenation of the strings in separated
+
+    Examples
+    ----------
+    >>> ob_join(['a','b','c','d'])
+    'abcd'
+    """
+    joined = ""
+    for i in separated:
+        joined += i
+    return joined
+
+def standard_product(word, n=None):
+    """
+    if word = $(i_1,\ldots,i_d)$, performs $s_{i_1}\circ\cdots\circs_{i_d}(abcd...)$
+
+    Parameters
+    ----------
+    word : list or tuple
+        the generators whose prodct to take
+
+    n : int (optional)
+        S_n in which the product will take place
+        defaults to the smallest possible n
+
+    Returns
+    ----------
+    str
+        the element of S_n corresponding to the product of the generators in word
+        (as the left action of the generators on abcd...)
+
+    Examples
+    ----------
+    >>> standard_product([1])
+    'ba'
+
+    >>> standard_product([1],4)
+    'bacd'
+
+    >>> standard_product((3,2,1))
+    'bcda'
+    """
+    if n == None:
+        n = identify_n(word)
+
+    to_apply = list(word)
+    to_apply.reverse()
+    element = list(ascii_lowercase[:n])
+    for i in to_apply:
+        if i < 0 or i >= n:
+            raise ValueError("{} not between 0 and {}".format(i,n))
+        if i==0:
+            pass
+        else:
+            element[i-1],element[i]=element[i],element[i-1]
+    return ob_join(element)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
